d2e527bc3d00ecb589497a48d0fa1c4ce5827c2e
Sventimir <svantimir@gmail.com>
2024-01-18 17:19:18 +0100

    Add column headers to the partial report.

--- test/itn_uptime_analyzer/aggregate.py
+++ test/itn_uptime_analyzer/aggregate.py
@@ -1,12 +1,17 @@
 import os
+import re
 import csv
 import sys
 from collections import defaultdict
 
 OUTPUT_SUBDIR = 'output'
+FILENAME_RE = re.compile(
+  'summary_(\d{4}-\d{2}-\d{2})T(\d{2}):\d{2}:\d{2}-\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.csv'
+)
 
 def process_csv_directory(directory):
-    data = defaultdict(list)
+    data = defaultdict(lambda: defaultdict(float))
+    columns = set()
     output_data = {}
 
     csv_files = [file for file in os.listdir(directory) if file.endswith(".csv")]
@@ -15,6 +20,12 @@ def process_csv_directory(directory):
     base_for_avg = len(csv_files)
 
     for csv_file in csv_files:
+        match = FILENAME_RE.match(csv_file)
+        if not match:
+            print(f"Skipping file: {csv_file}")
+            continue
+        period = match.group(1) + ("_AM" if match.group(2) == "00" else "_PM")
+        columns.add(period)
         print(f"Processing file: {csv_file}")
         with open(os.path.join(directory, csv_file), 'r') as file:
             csv_reader = csv.reader(file, delimiter=';')
@@ -27,20 +38,24 @@ def process_csv_directory(directory):
 
             for row in csv_reader:
                 key, value = row[0], float(row[1])
-                data[key].append(value)
+                data[key][period] = value
+
+    columns = sorted(columns)
 
     # Save in dir/output as csv file
     partial_output_filename = os.path.join(directory, OUTPUT_SUBDIR, f"{directory}.partials.csv")
     with open(partial_output_filename, 'w') as partial_output_file:
-        for key, values in data.items():
+        partial_output_file.write(f"BP key: {', '.join(columns)}\n")
+        for key in data.keys():
+            values = [data[key][column] for column in columns]
             print(f"{key}: {values}")
             partial_output_file.write(f"{key}: {', '.join(map(str, values))}\n")
 
-    for key, values in data.items():
+    for key in data.keys():
         # Assumes correct input, no dups etc.
-        submited_values = len(values)
-        avg_value = round(sum(values) / max(base_for_avg, submited_values), 2)
-        output_data[key] = {'avg_value': avg_value, 'submitted': len(values)}
+        submitted_values = len(data[key])
+        avg_value = round(sum(data[key].values()) / max(base_for_avg, submitted_values), 2)
+        output_data[key] = {'avg_value': avg_value, 'submitted': submitted_values}
 
     sorted_output_data = dict(sorted(output_data.items(), key=lambda item: item[1]['avg_value']))
 
