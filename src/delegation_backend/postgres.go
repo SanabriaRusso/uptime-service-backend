37dfb8255e9dc02ebf51dde0b52b70f430a49f99
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-04-25 16:25:56 +0200

    update insert logic when snark_work missing and handle uq_submissions_submitter_date unique index

--- src/delegation_backend/postgres.go
+++ src/delegation_backend/postgres.go
@@ -27,6 +27,14 @@ func NewPostgreSQL(cfg *PostgreSQLConfig) (*sql.DB, error) {
 }
 
 func (ctx *PostgreSQLContext) insertSubmission(submission *Submission) error {
+	// if SnarkWork is empty, do not insert it into the database
+	if len(submission.SnarkWork) == 0 {
+		return ctx.insertSubmissionWithoutSnarkWork(submission)
+	}
+	return ctx.insertSubmissionWithSnarkWork(submission)
+}
+
+func (ctx *PostgreSQLContext) insertSubmissionWithoutSnarkWork(submission *Submission) error {
 	query := `INSERT INTO submissions 
 				(submitted_at_date, 
 				 submitted_at, 
@@ -35,14 +43,33 @@ func (ctx *PostgreSQLContext) insertSubmission(submission *Submission) error {
 				 block_hash, 
 				 remote_addr, 
 				 peer_id, 
-				 snark_work, 
 				 graphql_control_port,
 				 built_with_commit_sha)
-			   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
+			   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
 	_, err := ctx.DB.Exec(query, submission.SubmittedAtDate, submission.SubmittedAt,
 		submission.Submitter, submission.CreatedAt, submission.BlockHash,
-		submission.RemoteAddr, submission.PeerId, submission.SnarkWork,
-		submission.GraphqlControlPort, submission.BuiltWithCommitSha)
+		submission.RemoteAddr, submission.PeerId, submission.GraphqlControlPort,
+		submission.BuiltWithCommitSha)
+	return err
+}
+
+func (ctx *PostgreSQLContext) insertSubmissionWithSnarkWork(submission *Submission) error {
+	query := `INSERT INTO submissions 
+				(submitted_at_date, 
+				submitted_at, 
+				submitter, 
+				created_at, 
+				block_hash, 
+				remote_addr, 
+				peer_id, 
+				graphql_control_port,
+				built_with_commit_sha,
+				snark_work) 
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
+	_, err := ctx.DB.Exec(query, submission.SubmittedAtDate, submission.SubmittedAt,
+		submission.Submitter, submission.CreatedAt, submission.BlockHash,
+		submission.RemoteAddr, submission.PeerId, submission.GraphqlControlPort,
+		submission.BuiltWithCommitSha, submission.SnarkWork)
 	return err
 }
 
@@ -54,6 +81,12 @@ func (ctx *PostgreSQLContext) PostgreSQLSave(objs ObjectsToSave) {
 	}
 
 	if err := ctx.insertSubmission(submissionToSave); err != nil {
+		// if err contains uq_submissions_submitter_date then we can ignore it
+		// because it means that the submission is already in the database
+		if err.Error() == "pq: duplicate key value violates unique constraint \"uq_submissions_submitter_date\"" {
+			ctx.Log.Infof("PostgreSQLSave: Submission for submitter: %v at %v already exists", submissionToSave.Submitter, submissionToSave.SubmittedAt)
+			return
+		}
 		ctx.Log.Errorf("PostgreSQLSave: Error saving submission to PostgreSQL: %v", err)
 	} else {
 		ctx.Log.Infof("PostgreSQLSave: Successfully saved submission for submitter: %v at %v", submissionToSave.Submitter, submissionToSave.SubmittedAt)
